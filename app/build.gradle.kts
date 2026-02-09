0e0b737294a83c1ef40bc9ec9acc12ff39cb9d59
Jatin2102786 <jatin@o7tec.com>
2026-02-09 16:09:02 +0530

    Authentication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -55,4 +56,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+    implementation("com.google.firebase:firebase-auth")
+
 }
---
+++ app/src/main/java/com/o7solutions/android_2_months/Firebase/RegisterActivity.kt
@@ -0,0 +1,65 @@
+package com.o7solutions.android_2_months.Firebase
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.o7solutions.android_2_months.BottomNavigation.BottomNavigationActivity
+import com.o7solutions.android_2_months.R
+import com.o7solutions.android_2_months.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+
+    lateinit var binding: ActivityRegisterBinding
+
+
+    var auth = FirebaseAuth.getInstance()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.submit.setOnClickListener {
+
+            val email = binding.emailTV.text.toString()
+            val password = binding.passwordTV.text.toString()
+
+
+            if(email.isEmpty()) {
+                Toast.makeText(this, "Please fill email", Toast.LENGTH_SHORT).show()
+            }  else if(password.isEmpty()) {
+                Toast.makeText(this, "Please fill password", Toast.LENGTH_SHORT).show()
+            } else {
+
+
+                auth.createUserWithEmailAndPassword(email,password)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Account created successfully!", Toast.LENGTH_SHORT).show()
+
+                        val intent = Intent(this, BottomNavigationActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    .addOnFailureListener { e ->
+
+                        Toast.makeText(this, e.localizedMessage, Toast.LENGTH_SHORT).show()
+
+                    }
+            }
+
+
+        }
+    }
+}
